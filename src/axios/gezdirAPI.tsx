8ae8b57615d8e735e8da51ce9db649084fd63ede
Muhammed Salih Tatar <05200000764@ogrenci.ege.edu.tr>
2023-06-12 23:45:55 +0300

    Facilator Module sayfası oluşturuldu.

--- src/axios/gezdirAPI.tsx
+++ src/axios/gezdirAPI.tsx
@@ -10,11 +10,9 @@ const getPreferencedLocations = () => {return axios.get(`https://localhost:5002/
 
 const getLocationByPlaceId = (placeId: number) => {return axios.get(`https://localhost:5002/api/locations/detail/${placeId}`);}//bir placeId ye ait detayları döner
 
-
 const getAllLocationsDummy = () => {return Promise.resolve({ status : 200, data:dummyData});}//önerilen mekanları döner
 const getLocationByPlaceIdDummy = (placeId: number) =>{return Promise.resolve({ status : 200, data:(dummyDetail.filter(x => x.placeId == placeId)[0]??dummyDetail[0])});}//bir placeId ye ait detayları döner
 
+const getImageCaption = (imageName: string) => {return axios.get(`http://127.0.0.1:5999/get_image_caption?image_name=${imageName}`);}//görüntü altyazılama get
 
-
-
-export {getTopPlaces, getAllLocations, getPreferencedLocations, getLocationByPlaceId,getAllLocationsDummy,getLocationByPlaceIdDummy};
+export {getTopPlaces, getAllLocations, getPreferencedLocations, getLocationByPlaceId, getAllLocationsDummy, getLocationByPlaceIdDummy, getImageCaption};
